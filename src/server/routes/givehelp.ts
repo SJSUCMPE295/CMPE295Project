46a1ad438f0c0dbff3fdcb79c04b52bac999638b
supriyasethi <supriya.sethi@outlook.com>
2021-10-22 01:56:33 -0700

    give help service

--- src/server/routes/givehelp.ts
+++ src/server/routes/givehelp.ts
@@ -30,4 +30,29 @@ router.post('/resource', async (_req, res) => {
     }
 });
 
+router.post('/service', async (_req, res) => {
+    const servicedata = new Services({
+        UserId: _req.body.UserId,
+        Service_Name: _req.body.Service_Name,
+        Category: _req.body.Category,
+        Description: _req.body.Description,
+        Phone_Number: _req.body.Phone_Number,
+        AddressId: _req.body.AddressId,
+        Availability: _req.body.Availability,
+    });
+    try {
+        await servicedata.save((error, data) => {
+            if (error) {
+                console.log('error', error);
+                res.json(500).send(error);
+            } else {
+                console.log('data', data);
+                res.status(200).json(data);
+            }
+        });
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
 export default router;
