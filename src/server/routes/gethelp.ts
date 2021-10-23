3e3a673b3dcc12424367dc12a66ed8d252d5738d
Arturo Montoya <montoya332@live.com>
2021-10-22 17:54:20 -0700

    server unit test example

--- src/server/routes/gethelp.ts
+++ src/server/routes/gethelp.ts
@@ -1,29 +1,34 @@
 import { Router } from 'express';
 const router = Router();
 // Create a new Resource
-router.post('/', (_req, res) => {
+export const createHandler = async (_req, res) => {
     res.send({ hello: 'Create a new Resource' });
-});
+};
+router.post('/', createHandler);
 
 // Retrieve all Resources
-router.get('/', (_req, res) => {
+export const getAllHandler = async (_req, res) => {
     res.send({ hello: 'Retrieve all Resources' });
-});
+};
+router.get('/', getAllHandler);
 
 // Retrieve a single Resource with id
-router.get('/:id', (_req, res) => {
+export const getItemHandler = async (_req, res) => {
     res.send({ hello: 'Retrieve a single Resource with id' });
-});
+};
+router.get('/:id', getItemHandler);
 
 // Update a Resource with id
-router.put('/:id', (_req, res) => {
+export const updateItemHandler = async (_req, res) => {
     res.send({ hello: 'Update a Resource with id' });
-});
+};
+router.put('/:id', updateItemHandler);
 
 // Delete a Resource with id
-router.delete('/:id', (_req, res) => {
+export const deleteItemHandler = async (_req, res) => {
     res.send({ hello: 'Delete a Resource with id' });
-});
+};
+router.delete('/:id', deleteItemHandler);
 
 export default router;
 //app.use('/api/gethelp', router);
