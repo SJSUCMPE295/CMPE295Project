94d61f946a32e5785e7e78ef0bfb7c05807a67e1
Arturo Montoya <montoya332@live.com>
2021-11-02 21:57:33 -0700

    bigger screen size

--- src/shared/pages/__tests__/app-pages.snapshots.ts
+++ src/shared/pages/__tests__/app-pages.snapshots.ts
@@ -2,7 +2,15 @@ import puppeteer from 'puppeteer';
 describe('Visual Regression Tests', () => {
     let browser;
     beforeEach(async () => {
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            headless: true,
+            ignoreHTTPSErrors: true,
+            args: [`--window-size=1920,1080`],
+            defaultViewport: {
+                width: 1920,
+                height: 1080,
+            },
+        });
     });
     it('renders correctly', async () => {
         const page = await browser.newPage();
