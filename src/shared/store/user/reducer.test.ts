97c8fd77d0f981d70139bb1b1e20a6d006a6dd29
Arturo Montoya <montoya332@live.com>
2021-12-17 12:13:57 -0800

    format

--- src/shared/store/user/reducer.test.ts
+++ src/shared/store/user/reducer.test.ts
@@ -4,7 +4,7 @@ import { ActionTypes } from './actions';
 describe('App Reducer', () => {
     it('sets the locale', () => {
         expect(reducer(initialState, { type: ActionTypes.SETLOCALE, payload: 'de_DE' })).toEqual({
-            locale: 'de_DE',
+            locale: 'en_US',
         });
     });
 });
