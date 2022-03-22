d4f674fef587a8e4537a43a6733b1e30d8061968
Sneha Thomas <sneha.thomas@sjsu.edu>
2022-03-21 17:41:52 -0700

    gethelpcardchanges

---
+++ src/shared/components/gethelp/map.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "google-map": string;
+  readonly "map-h2": string;
+  readonly "pin": string;
+  readonly "pin-icon": string;
+  readonly "pin-text": string;
+};
+export = styles;
+
