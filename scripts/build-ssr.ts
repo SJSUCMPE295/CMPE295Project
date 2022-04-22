d9d66db683cf25dd7fa403f9ea66bc57f030a600
Arturo Montoya <montoya332@live.com>
2022-04-21 22:48:47 -0700

    kill after build success

--- scripts/build-ssr.ts
+++ scripts/build-ssr.ts
@@ -76,6 +76,7 @@ const build = async () => {
         await clientPromise;
         // await generateStaticHTML();
         logMessage('Done!', 'info');
+        process.exit(1);
     } catch (error) {
         logMessage(error, 'error');
     }
